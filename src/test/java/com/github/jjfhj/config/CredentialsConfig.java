21649de29ba87afe0c59490d798de168f86f4d56
Karina <aiwpront@gmail.com>
2022-01-19 22:42:13 +0400

    added CredentialsConfig

---
+++ src/test/java/com/github/jjfhj/config/CredentialsConfig.java
@@ -0,0 +1,16 @@
+package com.github.jjfhj.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({"classpath:config/credentials.properties"})
+public interface CredentialsConfig extends Config {
+
+    @Key("user")
+    String user();
+
+    @Key("key")
+    String key();
+
+    @Key("app")
+    String app();
+}
