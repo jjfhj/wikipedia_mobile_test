dd6e45bda99c4f95f55d80f4d2864b1f8b52f352
Karina <aiwpront@gmail.com>
2022-01-27 01:16:06 +0400

    added Tag

--- src/test/java/com/github/jjfhj/tests/BrowserstackTests.java
+++ src/test/java/com/github/jjfhj/tests/BrowserstackTests.java
@@ -2,6 +2,7 @@ package com.github.jjfhj.tests;
 
 import io.appium.java_client.MobileBy;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
@@ -11,6 +12,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
 
+@Tag("browserstack")
 public class BrowserstackTests extends TestBase {
 
     @Test
