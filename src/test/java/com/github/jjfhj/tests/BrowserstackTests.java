5c89d88b7d0ad9f89ba0e67240ca9a5485884cb7
Karina <aiwpront@gmail.com>
2022-01-27 00:51:57 +0400

    renamed BrowserstackTests

--- src/test/java/com/github/jjfhj/tests/BrowserstackTests.java
+++ src/test/java/com/github/jjfhj/tests/BrowserstackTests.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
 
-public class AndroidTests extends TestBase {
+public class BrowserstackTests extends TestBase {
 
     @Test
     @DisplayName("Отображение результатов поиска")
