669a47517c2650d1005e172287381986c04b8bcd
Karina <aiwpront@gmail.com>
2022-01-26 18:34:32 +0400

    added Emulation config

--- src/test/java/com/github/jjfhj/drivers/EmulationMobileDriver.java
+++ src/test/java/com/github/jjfhj/drivers/EmulationMobileDriver.java
@@ -1,7 +1,9 @@
 package com.github.jjfhj.drivers;
 
 import com.codeborne.selenide.WebDriverProvider;
+import com.github.jjfhj.config.EmulationConfig;
 import io.appium.java_client.android.AndroidDriver;
+import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -12,9 +14,21 @@ import static com.github.jjfhj.utils.FileUtils.getAbsolutePath;
 
 public class EmulationMobileDriver implements WebDriverProvider {
 
+    private static final EmulationConfig emulation = ConfigFactory.create(EmulationConfig.class, System.getProperties());
+    private static final String deviceName = emulation.deviceName();
+    private static final String platformName = emulation.platformName();
+    private static final String version = emulation.version();
+    private static final String locale = emulation.locale();
+    private static final String language = emulation.language();
+    private static final String appPackage = emulation.appPackage();
+    private static final String appActivity = emulation.appActivity();
+    private static final String app = emulation.app();
+    private static final String url = emulation.remoteURL();
+
+
     public static URL getBrowserstackUrl() {
         try {
-            return new URL("http://127.0.0.1:4723/wd/hub");
+            return new URL(url);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
@@ -23,18 +37,18 @@ public class EmulationMobileDriver implements WebDriverProvider {
     @Override
     public WebDriver createDriver(DesiredCapabilities desiredCapabilities) {
 
-        desiredCapabilities.setCapability("platformName", "android");
-        desiredCapabilities.setCapability("deviceName", "Pixel_4_API_30");
-        desiredCapabilities.setCapability("version", "11.0");
+        desiredCapabilities.setCapability("deviceName", deviceName);
+        desiredCapabilities.setCapability("platformName", platformName);
+        desiredCapabilities.setCapability("version", version);
 
-        desiredCapabilities.setCapability("locale", "en");
-        desiredCapabilities.setCapability("language", "en");
+        desiredCapabilities.setCapability("locale", locale);
+        desiredCapabilities.setCapability("language", language);
 
-        desiredCapabilities.setCapability("appPackage", "org.wikipedia.alpha");
-        desiredCapabilities.setCapability("appActivity", "org.wikipedia.main.MainActivity");
+        desiredCapabilities.setCapability("appPackage", appPackage);
+        desiredCapabilities.setCapability("appActivity", appActivity);
 
         desiredCapabilities.setCapability("app",
-                getAbsolutePath("src/test/resources/apk/app-alpha-universal-release.apk"));
+                getAbsolutePath(app));
 
         return new AndroidDriver(getBrowserstackUrl(), desiredCapabilities);
     }
